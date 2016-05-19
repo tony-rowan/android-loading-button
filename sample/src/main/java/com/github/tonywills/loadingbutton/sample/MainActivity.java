5007e15fbc88af4e22bba4370f82e2045ef33a9a
Tony Williams <92antwilliams@gmail.com>
2016-05-19 22:26:38 +0100

    Much simpler example app

--- sample/src/main/java/com/github/tonywills/loadingbutton/sample/MainActivity.java
+++ sample/src/main/java/com/github/tonywills/loadingbutton/sample/MainActivity.java
@@ -1,96 +1,39 @@
 package com.github.tonywills.loadingbutton.sample;
 
-import android.content.Context;
-import android.content.res.Resources.Theme;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ThemedSpinnerAdapter;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
+
+import com.github.tonywills.loadingbutton.LoadingButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoadingButton loadingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupActionBar();
+        loadingButton = (LoadingButton) findViewById(R.id.loading_button);
+        loadingButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                loadingButton.setLoading(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override public void run() {
+                        loadingButton.setLoading(false);
+                    }
+                }, 3000);
+            }
+        });
 
-        // Setup toolbar
+    }
+
+    private void setupActionBar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        setupSpinner(toolbar);
-    }
-
-    private void setupSpinner(Toolbar toolbar) {
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        assert spinner != null;
-        assert toolbar != null;
-        spinner.setAdapter(new MyAdapter(toolbar.getContext(),
-                new String[] { "Loading Button" })
-        );
-
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-            @Override public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.main_content, getFragment(position))
-                        .commit();
-            }
-
-            @Override public void onNothingSelected(AdapterView<?> parent) {}
-        });
-    }
-
-    private Fragment getFragment(int position) {
-        switch (position) {
-            case 0:
-                return LoadingButtonExampleFragment.newInstance();
-
-            default:
-                throw new IllegalArgumentException("Unknown position " + position);
-        }
-    }
-
-
-    private static class MyAdapter extends ArrayAdapter<String> implements ThemedSpinnerAdapter {
-        private final ThemedSpinnerAdapter.Helper mDropDownHelper;
-
-        public MyAdapter(Context context, String[] objects) {
-            super(context, android.R.layout.simple_list_item_1, objects);
-            mDropDownHelper = new ThemedSpinnerAdapter.Helper(context);
-        }
-
-        @Override public View getDropDownView(int position, View convertView, ViewGroup parent) {
-            View view;
-
-            if (convertView == null) {
-                LayoutInflater inflater = mDropDownHelper.getDropDownViewInflater();
-                view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
-            } else {
-                view = convertView;
-            }
-
-            TextView textView = (TextView) view.findViewById(android.R.id.text1);
-            textView.setText(getItem(position));
-
-            return view;
-        }
-
-        @Override public Theme getDropDownViewTheme() {
-            return mDropDownHelper.getDropDownViewTheme();
-        }
-
-        @Override public void setDropDownViewTheme(Theme theme) {
-            mDropDownHelper.setDropDownViewTheme(theme);
-        }
+        setTitle("Loading Button Example");
     }
 }
